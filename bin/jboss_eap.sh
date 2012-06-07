18a10f633472275b3d1b09502f009ee3c19c461f
Jeffrey Bride <jbride2001@yahoo.com>
2012-06-07 17:13:01 -0400

    now working in standalone mode again

--- bin/jboss_eap.sh
+++ bin/jboss_eap.sh
@@ -1,38 +1,24 @@
 #!/bin/sh
 
-export jbossDomainBaseDir=$2
-export domainConfig=$3
-
+export command=$1
+export serverConfig=$2
 export cliPort=$2
-export node=$3
+export jbossServerBaseDir=$3
+export jbossSocketBindingPortOffset=$4
 
 start() {
-    if [ "x$jbossDomainBaseDir" = "x" ]; then
-        jbossDomainBaseDir=domain
-    fi
-    if [ "x$domainConfig" = "x" ]; then
-        domainConfig=domain.xml
-    fi
-    echo -en $"Starting jboss daemon w/ following command line args: \n\tjboss.bind.address = $HOSTNAME\n\t-bmanagement = $HOSTNAME\n\tjboss.domain.base.dir= $jbossDomainBaseDir\n\tdomainConfig=$domainConfig\n"
-    sleep 1 
-    cd $JBOSS_HOME
-    rm nohup.out
-    nohup ./bin/domain.sh -b=$HOSTNAME -bmanagement=$HOSTNAME -Djboss.domain.base.dir=$jbossDomainBaseDir -Ddomain-config=$domainConfig &
-    sleep 10 
+    echo -en $"Starting jboss daemon w/ following command line args: \n\tserver-config = $serverConfig\n\tjboss.server.base.dir = $jbossServerBaseDir \n\tjboss.socket.binding.port-offset = $jbossSocketBindingPortOffset \n\t"
+     cd $JBOSS_HOME
+    nohup ./bin/standalone.sh -b=$HOSTNAME -bmanagement=$HOSTNAME --server-config=$serverConfig -Djboss.server.base.dir=$jbossServerBaseDir -Djboss.socket.binding.port-offset=$jbossSocketBindingPortOffset &
+     sleep 10 
 }
 
 stop() {
-    if [ "x$cliPort" = "x" ]; then
-        cliPort=9999
-    fi
-    if [ "x$node" = "x" ]; then
-        node=master
-    fi
-
-    echo -en $"stopping the following jboss node: $node\n"
+    echo -en $"stopping jboss daemon: "
     cd $JBOSS_HOME
-    ./bin/jboss-cli.sh --connect --controller=$HOSTNAME:$cliPort --command=/host=$node:shutdown
+    ./bin/jboss-cli.sh --connect --controller=$HOSTNAME:$cliPort --command=:shutdown
     echo
+    rm nohup.out
     sleep 2
 }
 
