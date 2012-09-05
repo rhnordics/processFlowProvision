c7f640b0c64cbcce23689bf1bb25ff4d65c29a6c
Jeffrey Bride <jbride@zareason.com>
2012-09-05 08:13:23 -0400

    latest

--- conf/mysql/processFlowProvision.sql
+++ conf/mysql/processFlowProvision.sql
@@ -2,12 +2,12 @@
 -- JA Bride :  22 June 2012
 
 create database if not exists guvnor;
-grant all on guvnor.* to 'guvnor'@${brmsWebs.db.ip.addr} identified by 'guvnor';
+grant all on guvnor.* to 'guvnor'@'%' identified by 'guvnor';
 
 create database if not exists jbpm;
-grant all on jbpm.* to 'jbpm'@${brmsWebs.db.ip.addr} identified by 'jbpm';
+grant all on jbpm.* to 'jbpm'@'%' identified by 'jbpm';
 
 create database if not exists jbpm_bam;
-grant all on jbpm_bam.* to 'jbpm_bam'@${brmsWebs.db.ip.addr} identified by 'jbpm_bam';
+grant all on jbpm_bam.* to 'jbpm_bam'@'%' identified by 'jbpm_bam';
 
 flush privileges;
