9d98b0b5a2a5768ea6013fbf559f76c804f4e582
Jeffrey Bride <jbride@zareason.com>
2012-09-05 04:57:50 -0400

    updated sql script from mysql

--- conf/mysql/processFlowProvision.sql
+++ conf/mysql/processFlowProvision.sql
@@ -3,6 +3,11 @@
 
 create database if not exists guvnor;
 grant all on guvnor.* to 'guvnor'@${brmsWebs.db.ip.addr} identified by 'guvnor';
--- set password for guvnor = password('guvnor');
+
+create database if not exists jbpm;
+grant all on jbpm.* to 'jbpm'@${brmsWebs.db.ip.addr} identified by 'jbpm';
+
+create database if not exists jbpm_bam;
+grant all on jbpm_bam.* to 'jbpm_bam'@${brmsWebs.db.ip.addr} identified by 'jbpm_bam';
 
 flush privileges;
